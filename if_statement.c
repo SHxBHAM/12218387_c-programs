25b0d1b2a9249fbadddd400b49c236ad967e3f4b
Shubham <shubhammishra41880@gmail.com>
2023-03-01 11:45:39 +0530

    IF STATEMENT

---
+++ if_statement.c
@@ -0,0 +1,12 @@
+//write a program to find wether given numnber is greater than 10
+#include<stdio.h>
+int main(){
+	int a;
+	printf("enter value: ");
+	scanf("%d",&a);
+	if(a>10){
+		printf("%d is greater than 10",a);
+	}
+	return 0;
+	
+}
