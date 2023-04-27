5c321f5c224419ad0ed646a06d52c8348d1ac434
Shubham <shubhammishra41880@gmail.com>
2023-04-27 12:28:39 +0530

    class notes of 27-04-23

---
+++ class notes 27-04-23.c
@@ -0,0 +1,64 @@
+//#include<stdio.h>
+//int main(){
+//	int arr[] = {10,20,30,40,50,60,70};
+//	int *ptr = arr;
+//	int i;
+//	for(i=0;i<7;i++){
+//		printf("%d \n",*(ptr+i));
+//	}
+//	printf("adresses are \n");
+//	for(i=0;i<7;i++){
+//		printf("%u \n",(ptr+i));
+//	}
+//	
+//	return 0;
+//}
+//WAP to read and display elements of !D array using pointer to an array
+//#include<stdio.h>
+//int main(){
+//	int i,n;
+//	int a[10];
+//	int *parr = a;
+//	printf("Enter no. of elements \n");
+//	scanf("%d \n",&n);
+//	printf("ENTER THE ELEMENTS \n");
+//	for(i=0;i<n;i++){
+//		scanf("%d",parr+i);
+//	}
+//	printf("Display elements \n");
+//	for (i=0;i<n;i++){
+//		printf("%d \n", *parr+i);
+//	}
+//	return 0;
+//}
+//WAP TO FIND THE SUM AND MEAN OF 1D array element using pointer to array 
+//#include<stdio.h>
+//int main(){
+//	
+//}
+//Strcmp
+//strncmp
+//stricmp
+//string conversion functions
+//#include<stdlib.h>
+//it is used to convert string of digits to integer and floating point value and vice versa 
+//create a table
+//func1 atof = it converts the string to double 
+//func2 atoi = converts the string into integer
+//func3 atol = converts the string to long integer 
+//func4 itoa = converts integer value to string 
+
+//converting from float to string 
+//#include<stdio.h>
+//void ftoa1(float f1,char s[])
+//{
+//sprintf(s,"%f",f1);// s is array, %f format specifier and f1 is float variable
+//}
+//int main()
+//{
+//char str[20];
+//float f=12.340000;
+//ftoa1(f,str);//it means convert float value f to string and store it in str// function call//
+//printf("\n%s",str);
+//return 0;
+//}
